472abdb0a30a8881061867979d2e7898834148f8
nata <korovina-nataliya@mail.ru>
2019-07-19 18:18:53 +0300

    fix

--- src/Classes/Select.php
+++ src/Classes/Select.php
@@ -7,7 +7,7 @@ namespace Query;
 class Select
 {
     public function __construct($url, $apiKey, $table, $type, $site)
-    {
+    { 
         switch ($type){
             case 'orders':
                 $request = new CreateOrder(
